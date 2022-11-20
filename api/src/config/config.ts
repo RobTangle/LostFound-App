3c57a6e57258f641a35119917f1a516a8b8addcc
Robert0101 <rob.tangled@gmail.com>
2022-11-20 09:57:26 -0300

    build/Crear archivo config

---
+++ api/src/config/config.ts
@@ -0,0 +1,15 @@
+require("dotenv").config();
+const PORT = process.env.PORT || 3001;
+
+const MONGO_USERNAME = process.env.MONGO_USERNAME || "";
+const MONGO_PASSWORD = process.env.MONGO_PASSWORD || "";
+const MONGO_URI = `mongodb+srv://${MONGO_USERNAME}:${MONGO_PASSWORD}@docssaviourappdatabase.b0wlnau.mongodb.net/?retryWrites=true&w=majority`;
+
+export const config = {
+  mongo: {
+    uri: MONGO_URI,
+  },
+  server: {
+    port: PORT,
+  },
+};
