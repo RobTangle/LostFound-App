b43edcd31e984126b12ebc607991d56d2af4fc11
Robert0101 <rob.tangled@gmail.com>
2022-11-21 22:38:49 -0300

    docs/Agregar comentarios sobre suscripciones de alertas

--- api/src/mongoDB/models/User.ts
+++ api/src/mongoDB/models/User.ts
@@ -18,8 +18,14 @@ export const userSchema: Schema = new Schema<IUser>(
       lowercase: true,
       minlength: 6,
     },
+    // Suscripciones de alertas. [{query}, {query}] MAX 5.
     profile_img: { type: String, required: false },
     posts: [{ type: String, ref: "Post" }],
   },
   { timestamps: true }
 );
+
+// Pensar sistema de alertas y forma de guardar las suscripciones de las alertas.
+// -La alerta es un objeto que representa una query?
+// - Una vez que se crea un posteo, voy con los datos del nuevo posteo a buscar matches por la collection de alertas?
+// Qué sistema es más eficiente? Pensar cantidades de alertas y cantidades de posteos nuevos y sus relaciones en queries en una dirección u otra.
