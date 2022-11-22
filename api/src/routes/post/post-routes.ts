bf97d9330392c65ef9735527af59e515171e4498
Robert0101 <rob.tangled@gmail.com>
2022-11-22 01:07:15 -0300

    pro/Dejar a punto ruta GET '/' by query

--- api/src/routes/post/post-routes.ts
+++ api/src/routes/post/post-routes.ts
@@ -3,12 +3,13 @@ import { Post, User } from "../../mongoDB";
 import { IPost } from "../../mongoDB/models/Post";
 import { validatePost } from "../../validators/post-validators";
 import { getUserByIdOrThrowError } from "../user/user-auxiliaries";
+import { searchPostsByQuery } from "./post-r-auxiliary";
 
 const router = Router();
 
 router.get("/allPosts", async (req: Request, res: Response) => {
   try {
-    const allPostsFromDB = await Post.find();
+    const allPostsFromDB = await Post.find().lean();
     return res.status(200).send(allPostsFromDB);
   } catch (error: any) {
     console.log(`Error en ruta /allPosts. ${error.message}`);
@@ -40,25 +41,13 @@ router.post("/newPost", async (req: Request, res: Response) => {
 
 // En el formulario del front, que hagan un chequeo de que las letras del nombre sean [a-zA-z-0-9-áéíóúÁÉÍÓÚÜüçÇñÑ] y que no se equivoquen de tilde con la invertida. Tenemos que pedir que el nombre sea idéntico a como figura en el documento.
 // Ya que descartamos la importancia de las tarjetas de crédito y le damos más importanci a pasaportes y DNI, el nombres siempre va a figurar completo. Y las tarjetas de crédito, la persona debería denunciarlas inmediatamente.
-//
 
-//  $and:[ {$or:[{nameOnDoc}, {numberOnDoc}]},{country_found: country}, {date_found: {$lte: dateLost}}]
 router.get("/", async (req: Request, res: Response) => {
   try {
-    // Agarro el req.auth.sub y busco si el usuario está verificado para autorizarlo a hacer la búsqueda ??? Metería bastante costo por sólo esto. Es necesario y debe ser tomada como medida de seguridad? Hay una forma mejor de chequear la autorización?
-    const nameOnDoc = req.query.name; // regex idéntica. //!PENSARLO BIEN
-    const numberOnDoc = req.query.number; //exactamente mismo número;
-    //parsear numberOnDoc para que tenga mismo filtro que al guardar el post.
-    const country = req.query.country; // exactamente el mismo country;
-    const dateLost = req.query.datelost; // dateLost tiene que ser menor que dateFound para que matchee.
-    // parsear dateLost para que coincida con el formato de la DB.
-    const postsFound = Post.find({
-      $and: [
-        { $or: [{ name_on_doc: nameOnDoc }, { number_on_doc: numberOnDoc }] },
-        { country_found: country },
-        { date_found: { $gte: dateLost } },
-      ],
-    });
+    // Agarro el req.auth.sub y busco si el usuario está verificado para autorizarlo a hacer la búsqueda:
+    // if (!req.auth?.verified) throw new Error (`The user account must be verified.`);
+    const postsFound = await searchPostsByQuery(req.query);
+    return res.status(200).send(postsFound);
   } catch (error: any) {
     console.log(`Error en GET "/". ${error.message}`);
     return res.status(400).send({ error: error.message });
