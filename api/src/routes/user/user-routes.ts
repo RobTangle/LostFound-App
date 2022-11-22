5aa12ddfa65bf41c1f319bbe9117aa21fcd075d6
Robert0101 <rob.tangled@gmail.com>
2022-11-22 16:46:49 -0300

    feat/Crear ruta PUT 'user/update'

--- api/src/routes/user/user-routes.ts
+++ api/src/routes/user/user-routes.ts
@@ -2,7 +2,14 @@ import { Router, Request, Response } from "express";
 import { Request as JWTRequest } from "express-jwt";
 import { User } from "../../mongoDB";
 import { IUser } from "../../mongoDB/models/User";
+import {
+  isStringBetweenXAndYCharsLong,
+  isValidString,
+  isValidURLImage,
+  stringContainsURLs,
+} from "../../validators/genericValidators";
 import { validateNewUser } from "../../validators/user-validators";
+import { getUserByIdOrThrowError } from "./user-auxiliaries";
 
 const router = Router();
 
@@ -25,6 +32,41 @@ router.post("/newUser", async (req: JWTRequest, res: Response) => {
     return res.status(200).send(newUser);
   } catch (error: any) {
     console.log(`Error en POST '/newUser. ${error.message}`);
+    return res.status(400).send({ error: error.message });
+  }
+});
+
+// UPDATE NAME AND/OR PROFILE_IMG :
+router.put("/update", async (req: JWTRequest, res: Response) => {
+  try {
+    // const _id = req.auth?.sub;
+    const _id = req.body._id; // TEMPORARY UNTIL JWT APPLIES
+    const { name, profile_img } = req.body;
+
+    const userInDB = await getUserByIdOrThrowError(_id);
+    if (name) {
+      if (
+        isStringBetweenXAndYCharsLong(2, 50, name) &&
+        !stringContainsURLs(name)
+      ) {
+        userInDB.name = name;
+      } else {
+        throw new Error(`El nombre ingresado "${name}" no es válido.`);
+      }
+    }
+    if (profile_img) {
+      if (isValidURLImage(profile_img)) {
+        userInDB.profile_img = profile_img;
+      } else {
+        throw new Error(
+          `La URL de imágen de perfíl ingresada "${profile_img}" no es válida.`
+        );
+      }
+    }
+    await userInDB.save();
+    return res.status(200).send(userInDB);
+  } catch (error: any) {
+    console.log(`Error en PUT 'user/update'. ${error.message}`);
     return res.status(400).send({ error: error.message });
   }
 });
