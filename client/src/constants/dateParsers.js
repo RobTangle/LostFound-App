67dca88d5e64fe42fc1d651cb5b39a55764851b0
Robert0101 <rob.tangled@gmail.com>
2022-11-26 09:56:15 -0300

    cambiar de directorio el archivo

--- client/src/constants/dateParsers.js
+++
@@ -1,8 +0,0 @@
-export function parseDateToSetMaxDate() {
-  // Toma la fecha actual y la parsea para generar una fecha formato yyyy-MM-dd, el cual es el formato que maneja el input date del formulario.
-  let dateNow = new Date();
-  let parsedToJSON = dateNow.toJSON();
-  let maxDateAllowed = parsedToJSON.split("T")[0];
-  return maxDateAllowed;
-  //ej: maxDateAllowed = '2022-11-26'
-}
