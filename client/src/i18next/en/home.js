9fa07e35985f0fa244f1f6c08468a434cdb22e3c
Tamara Antonella Frazzetta <tamarafrazzetta@gmail.com>
2022-11-29 11:40:35 -0300

    Add Banner translation

--- client/src/i18next/en/home.js
+++ client/src/i18next/en/home.js
@@ -2,4 +2,7 @@ export const home = {
   lostTip: "Lost your documents? Here are some tips that will help you",
   foundTip: "Did you find documents and want to publish them?",
   about: "About",
+  welcome: "Welcome to",
+  subtitle: "The best place to find your lost documents",
+  welcomeText:"or publish your found documents.",
 };
--- client/src/i18next/esp/home.js
+++ client/src/i18next/esp/home.js
@@ -2,4 +2,7 @@ export const home = {
   lostTip: "Perdiste documentos? Aquí unos tips que ayudarán en tu búsqueda",
   foundTip: "Encontraste documentos y quieres publicarlos?",
   about: "About",
+  welcome: "Bienvenido a",
+  subtitle: "El mejor lugar para encontrar tus documentos perdidos",
+  welcomeText: "o publicar los documentos que encontraste.",
 };
