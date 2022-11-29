2c38c5229c618ec5de7b4f548dcdd73f222eeed8
Tamara Antonella Frazzetta <tamarafrazzetta@gmail.com>
2022-11-29 18:13:22 -0300

    Add text translations

--- client/src/i18next/en/home.js
+++ client/src/i18next/en/home.js
@@ -2,6 +2,13 @@ export const home = {
   lostTip: "Lost your documents? Here are some tips that will help you",
   foundTip: "Did you find documents and want to publish them?",
   about: "About",
-  welcome:"Lost your wallet? Found a lost credit card? ",
-  welcomeText: "This site helps you to find what you’re looking for easily. Here you can post all the things that belong to you and others, who are missing ANYTHING! Make sure that your lost things get found by posting them with LostFound"
+  welcome: "Lost your wallet? Found a lost credit card? ",
+  welcomeText: "This site helps you to find what you’re looking for easily. Here you can post all the things that belong to you and others, who are missing ANYTHING! Make sure that your lost things get found by posting them with LostFound",
+  lostTips1: "LostFound offers a safe, secure way to post your lost items here. We will do our best to find them for you as quickly as possible.",
+  lostTips2: "Please make sure that you subscribe to our notifications to get an email as soon as someone finds your lost item.",
+  lostTips3: "It is mandatory to provide a valid email address to post your lost item. We will not share your email address with anyone.",
+  foundTips1: "LostFound offers a safe, secure way to post your found items here. We will do our best to find the owner as quickly as possible.",
+  foundTips2: "Please make sure that you write a detailed description of the item you found. This will help us to find the owner.",
+  foundTips3: "It is mandatory to provide a valid email address to post your found item. We will not share your email address with anyone.",
+
 };
