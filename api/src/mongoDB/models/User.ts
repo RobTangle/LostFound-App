047a7a82919ccfe35cb4e9b7c3505acb0a6bbba4
Robert0101 <rob.tangled@gmail.com>
2022-12-15 14:19:58 -0300

    schema/agregar trim: true a props

--- api/src/mongoDB/models/User.ts
+++ api/src/mongoDB/models/User.ts
@@ -22,18 +22,26 @@ export interface INewUser {
 export const userSchema: Schema = new Schema<IUser>(
   {
     _id: { type: String, required: true },
-    name: { type: String, required: true, minlength: 2, maxlength: 50 },
+    name: {
+      type: String,
+      required: true,
+      minlength: 2,
+      maxlength: 50,
+      trim: true,
+    },
     email: {
       type: String,
       required: true,
       lowercase: true,
       minlength: 6,
+      trim: true,
     },
     profile_img: {
       type: String,
       required: false,
       default:
         "https://www.pngall.com/wp-content/uploads/5/User-Profile-PNG-Image.png",
+      trim: true,
     },
     contacts: {
       type: [Number],
