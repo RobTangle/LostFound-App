ef64bfad42a2740806cdd0a608720b06c79351f9
Patricio Monpelat <patricio.monpelat@gmail.com>
2022-12-16 15:03:46 -0300

    cambio de nombre de variable para uso general y coincida

--- api/src/config/config.ts
+++ api/src/config/config.ts
@@ -1,8 +1,8 @@
 require("dotenv").config();
 const PORT = process.env.PORT || 3001;
 
-const MONGO_USERNAME = process.env.MONGO_USERNAME || "";
-const MONGO_PASSWORD = process.env.MONGO_PASSWORD || "";
+const MONGO_USERNAME = process.env.MONGO_TEAM_USERNAME || "";
+const MONGO_PASSWORD = process.env.MONGO_TEAM_PASSWORD || "";
 const MONGO_URI = `mongodb+srv://${MONGO_USERNAME}:${MONGO_PASSWORD}@docssaviourappdatabase.b0wlnau.mongodb.net/?retryWrites=true&w=majority`;
 
 export const config = {
