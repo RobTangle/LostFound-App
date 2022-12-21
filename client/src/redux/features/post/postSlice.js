0d36e3673191349579091fc5c6d19a1706d73d7f
Robert0101 <rob.tangled@gmail.com>
2022-12-21 10:33:44 -0300

    Desarrollar reducer para Post

--- client/src/redux/features/post/postSlice.js
+++ client/src/redux/features/post/postSlice.js
@@ -2,18 +2,34 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
   searchResults: [],
-  newPost: {},
+  newPost: { pure: true },
+  postDetail: { pure: true },
+  postUpdated: { pure: true },
+  deletedPost: { pure: true },
+  contactPostOwner: { pure: true },
 };
 
 export const postSlice = createSlice({
   name: "post",
   initialState,
   reducers: {
-    getSearch: (state, action) => {
+    setSearchResults: (state, action) => {
       state.searchResults = action.payload;
     },
-    postDocument: (state, action) => {
+    setNewPost: (state, action) => {
       state.newPost = action.payload;
+    },
+    setPostDetail: (state, action) => {
+      state.postDetail = action.payload;
+    },
+    setUpdatePost: (state, action) => {
+      state.postUpdated = action.payload;
+    },
+    setDeletedPost: (state, action) => {
+      state.deletedPost = action.payload;
+    },
+    setContactPostOwner: (state, action) => {
+      state.contactPostOwner = action.payload;
     },
   },
 });
--- client/src/redux/features/post/postThunk.js
+++ client/src/redux/features/post/postThunk.js
@@ -1,12 +1,20 @@
-import { getSearch, postDocument } from "./postSlice";
+import {
+  setSearchResults,
+  setNewPost,
+  setPostDetail,
+  setUpdatePost,
+  setDeletedPost,
+  setContactPostOwner,
+} from "./postSlice";
 import axios from "axios";
 import {
+  URL_P_D_DELETE_POST,
+  URL_P_G_POST_INFO,
   URL_P_G_SEARCH_BY_QUERY,
   URL_P_PO_NEW_POST,
 } from "../../../constants/url";
 import { header } from "../../../constants/header";
 import Swal from "sweetalert2";
-
 
 export function createPost(post, token) {
   return async function (dispatch) {
@@ -28,6 +36,7 @@ export function createPost(post, token) {
             showConfirmButton: false,
             timer: 1500,
           });
+      return dispatch(setNewPost(response.data));
     } catch (error) {
       console.log(error.message);
       Swal.fire({
@@ -37,21 +46,99 @@ export function createPost(post, token) {
         showConfirmButton: false,
         timer: 1500,
       });
+      return dispatch(setNewPost({ error: error.message }));
     }
   };
 }
 
-export function searchPost({ name, number, country, date_lost }, token) {
+export function searchPost(
+  { name, number, country, date_lost, pag, lim, sortBy },
+  token
+) {
   return async function (dispatch) {
     try {
       let response = await axios.get(
         URL_P_G_SEARCH_BY_QUERY +
-          `?name=${name}&number=${number}&country=${country}&date_lost=${date_lost}`,
+          `?name=${name}&number=${number}&country=${country}&date_lost=${date_lost}&pag=${pag}&lim=${lim}&sortBy=${sortBy}`,
         header(token)
       );
-      return dispatch(getSearch(response.data));
+      return dispatch(setSearchResults(response.data));
     } catch (error) {
-      return dispatch(getSearch({ error: error.response?.data?.error }));
+      return dispatch(setSearchResults({ error: error.response?.data?.error }));
+    }
+  };
+}
+
+export function fetchPostDetail(post_id, token) {
+  return async function (dispatch) {
+    try {
+      let response = await axios.get(
+        URL_P_G_POST_INFO + post_id,
+        header(token)
+      );
+      return dispatch(setPostDetail(response.data));
+    } catch (error) {
+      return dispatch(setPostDetail({ error: error.message }));
+    }
+  };
+}
+
+export function updatePost(obj, post_id, token) {
+  return async function (dispatch) {
+    try {
+      let response = await axios.post(
+        URL_P_PA_UPDATE_POST + post_id,
+        obj,
+        header(token)
+      );
+      return dispatch(setUpdatePost(response.data));
+    } catch (error) {
+      return dispatch(setUpdatePost({ error: error.message }));
+    }
+  };
+}
+
+export function deletePost(post_id, token) {
+  return async function (dispatch) {
+    try {
+      let response = await axios.delete(
+        URL_P_D_DELETE_POST + post_id,
+        header(token)
+      );
+      return dispatch(setDeletedPost(response.data));
+    } catch (error) {
+      return dispatch(setDeletedPost({ error: error.message }));
+    }
+  };
+}
+
+export function contactPostOwner(post_id, token) {
+  return async function (dispatch) {
+    try {
+      let response = await axios.post(
+        URL_P_PO_CONTACT + post_id,
+        {},
+        header(token)
+      );
+      response.status === 202
+        ? Swal.fire({
+            position: "center",
+            icon: "success",
+            title: "Processing request. Check your email inbox.",
+            showConfirmButton: false,
+            timer: 1500,
+          })
+        : response.status >= 400 &&
+          Swal.fire({
+            position: "center",
+            icon: "error",
+            title: "Ups! Something went wrong.",
+            showConfirmButton: false,
+            timer: 1500,
+          });
+      return dispatch(setContactPostOwner(response.data));
+    } catch (error) {
+      return dispatch(setContactPostOwner({ error: error.message }));
     }
   };
 }
