20c9e8b9200883e250c3d27e246214549efee876
Robert0101 <rob.tangled@gmail.com>
2022-12-27 16:52:36 -0300

    Agregar textos para modal de edición de Post

--- client/src/i18next/esp/postForm.js
+++ client/src/i18next/esp/postForm.js
@@ -19,6 +19,10 @@ export const postForm = {
     "Información de contacto adicional a su email",
   subtitle: "Completá el formulario para publicar un aviso",
   submitButton: "Crear aviso",
+  //edit post form:
+  editTitle: "Editar publicación",
+  editSubtitle: "Modificá los campos que quieras editar",
+  editSubmitButton: "Guardar cambios",
   //errors:
   errorNameAndNumberFalsy:
     "Debe ingresar el nombre completo en el documento y/o el número en el documento.",
