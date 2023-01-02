ddc2c1cb43a34e498c3345e9b150a0b9f6d46245
Robert0101 <rob.tangled@gmail.com>
2023-01-02 19:28:59 -0300

    Mejorar los Tips

--- client/src/i18next/esp/home.js
+++ client/src/i18next/esp/home.js
@@ -3,20 +3,34 @@ export const home = {
   foundTip: "Encontraste documentos y quieres publicarlos?",
   about: "About",
   welcome: "Perdiste tu billetera? Encontraste una tarjeta de crédito? ",
-  welcomeText: "Este sitio te ayuda a encontrar lo que estás buscando fácilmente. Aquí puedes publicar todo aquello que perdiste o lo que encontraste. Asegúrate que tus cosas perdidas sean encontradas publicándolas con LostFound",
-  lostTips1: "LostFound ofrece una forma segura y segura de publicar tus artículos perdidos aquí. Haremos todo lo posible para encontrarlos lo más rápido posible.",
-  lostTips2: "Asegúrate de suscribirte a nuestras notificaciones para recibir un correo electrónico tan pronto como alguien encuentre tu artículo perdido.",
-  lostTips3: "Es obligatorio proporcionar una dirección de correo electrónico válida para publicar su artículo perdido. No compartiremos su dirección de correo electrónico con nadie.",
-  foundTips1: "LostFound ofrece una forma segura y segura de publicar sus artículos encontrados aquí. Haremos todo lo posible para encontrar al propietario lo más rápido posible.",
-  foundTips2: "Asegúrate de escribir una descripción detallada del artículo que encontraste. Esto nos ayudará a encontrar al propietario.",
-  foundTips3: "Es obligatorio proporcionar una dirección de correo electrónico válida para publicar su artículo encontrado. No compartiremos su dirección de correo electrónico con nadie.",
-  
-  
+  welcomeText:
+    "Este sitio te ayuda a encontrar lo que estás buscando fácilmente. Aquí puedes publicar todo aquello que perdiste o lo que encontraste. Asegúrate que tus cosas perdidas sean encontradas publicándolas con LostFound",
+  lostTips1:
+    "Es importante que completes con precisión los campos del formulario de búsqueda.",
+  lostTips2:
+    "Asegúrate de suscribirte a nuestras notificaciones para recibir un correo electrónico tan pronto como alguien encuentre tu artículo perdido. Puedes hacerlo desde tu perfíl de usuario.",
+  lostTips3:
+    "Al momento de completar el campo del número o nombre en el documento, ignoraremos cualquiér caracter no alfanumérico.",
+  lostTips4:
+    "Si no recuerdas exactamente la fecha en la que perdiste tu documento, redondea la fecha hacia el pasado. Es decir, si no tiene claro si perdiste tu documento en el mes de julio o junio, es mejor que digas que fue en junio. Esto es para ayudar al algoritmo de búsqueda.",
+  lostTips5:
+    "Las imágenes que cargan los usuarios son automáticamente alteradas para proteger los datos del dueño del documento. Puedes usar esta imágen para ver si el documento que encontraron se parece al tuyo o no.",
+  foundTips1:
+    "Si no recuerdas exactamente la fecha en la que encontraste el documento, redondea la fecha hacia el futuro. Es decir, si no tiene claro si encontraste el documento en el mes de julio o junio, es mejor que digas que fue en julio. Esto es para ayudar al algoritmo de búsqueda.",
+  foundTips2:
+    "Asegúrate de escribir una descripción detallada del artículo que encontraste. Esto nos ayudará a encontrar al propietario.",
+  foundTips3:
+    "Al momento de escribir el nombre en el documento, escríbelo tal cual figura en el documento.",
+  foundTips4:
+    "Si decides cargar una foto del documento, nosotros alteraremos la imágen para proteger la privacidad del dueño y cuidarnos de estafadores. La imágen del documento la podrá usar el dueño como una referencia de la apariencia del documento para poder tener una pista de si el documento publicado es realmente el suyo o no. Esto lo hacemos para prevenir estafas y al mismo tiempoo cuidar de la privacidad de las personas.",
+  foundTips5:
+    "Compartiremos tu email, nombre de usuario e información de contacto adicional con los usuarios que quieran contactarse contigo metiante tu publicación.",
   titleFind: "Perdiste documentos?",
-  subtitleFind: "No se preocupe, lo tenemos cubierto. Estamos aquí para ayudarlo a encontrar sus documentos perdidos.",
+  subtitleFind:
+    "No se preocupe, lo tenemos cubierto. Estamos aquí para ayudarlo a encontrar sus documentos perdidos.",
   btnFind: "Encontrarlos",
   titlePost: "Encontraste documentos y quieres publicarlos?",
-  subtitlePost: "Estamos aquí para ayudarlo a encontrar al propietario de los documentos que encontró.",
+  subtitlePost:
+    "Estamos aquí para ayudarlo a encontrar al propietario de los documentos que encontró.",
   btnPost: "Publicarlos",
-
 };
