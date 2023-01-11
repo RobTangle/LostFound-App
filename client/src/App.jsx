1f4b586311b48b42b62a91a64f5266c671000080
Patricio Monpelat <patricio.monpelat@gmail.com>
2023-01-11 09:51:11 -0300

    bar of progress

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,23 +1,24 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Landing, Home, Register, Search, Found, Profile, Error404, About, Services, Us, Contact } from "./views";
 import SubscriptionForm from "./components/SubscriptionForm/SubscriptionForm";
+import ProxyProgressBar from './components/ProxyProgressBar/ProxyProgressBar';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Landing />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/post" element={<Found />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/search" element={<Search />} />
-        <Route path="/subscription" element={<SubscriptionForm />} />
-        <Route path="*" element={<Error404 />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/services" element={<Services />} />
-        <Route path="/us" element={<Us />} />
-        <Route path="/contact" element={<Contact />} />
+        <Route path="/" element={<ProxyProgressBar componentToPassDown={<Landing />}/>} />
+        <Route path="/home" element={<ProxyProgressBar componentToPassDown={<Home />}/>} />
+        <Route path="/post" element={<ProxyProgressBar componentToPassDown={<Found />}/>} />
+        <Route path="/profile" element={<ProxyProgressBar componentToPassDown={<Profile />}/>} />
+        <Route path="/register" element={<ProxyProgressBar componentToPassDown={<Register />}/>} />
+        <Route path="/search" element={<ProxyProgressBar componentToPassDown={<Search />}/>} />
+        <Route path="/subscription" element={<ProxyProgressBar componentToPassDown={<SubscriptionForm />}/>} />
+        <Route path="/about" element={<ProxyProgressBar componentToPassDown={<About />}/>} />
+        <Route path="/services" element={<ProxyProgressBar componentToPassDown={<Services />}/>} />
+        <Route path="/us" element={<ProxyProgressBar componentToPassDown={<Us />}/>} />
+        <Route path="/contact" element={<ProxyProgressBar componentToPassDown={<Contact />}/>} />
+        <Route path="*" element={<ProxyProgressBar componentToPassDown={<Error404 />}/>} />
       </Routes>
     </BrowserRouter>
   );
---
+++ client/src/components/ProxyProgressBar/ProxyProgressBar.jsx
@@ -0,0 +1,32 @@
+import ProgressBar from "@badrap/bar-of-progress";
+import React from "react";
+
+const ProxyProgressBar = ({ componentToPassDown }) => {
+  
+  const progress = new ProgressBar({
+      // The size (height) of the progress bar.
+      // Numeric values get converted to px.
+      size: 4,
+      // Color of the progress bar.
+      // Also used for the glow around the bar.
+      color: "#6366f1",
+      // Class name used for the progress bar element.
+      className: "bar-of-progress",
+      // How many milliseconds to wait before the progress bar
+      // animation starts after calling .start().
+      delay: 0,
+  });
+
+  progress.start();
+
+  setTimeout(() => {
+      progress.finish();
+  }, 5000);
+  return (
+    <>
+     {componentToPassDown}  
+    </>
+  );
+}
+
+export default ProxyProgressBar;
